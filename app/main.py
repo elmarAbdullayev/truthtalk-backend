a953f06904d6ea08df6821afaa0cb6e1aad92826
elmarAbdullayev <114047390+elmarAbdullayev@users.noreply.github.com>
2026-03-21 13:00:06 +0100

    add agora-token-builder

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://truthtalk-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
